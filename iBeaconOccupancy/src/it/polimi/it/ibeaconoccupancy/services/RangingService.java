c2fa694f49a174837190a87da04d3e6a58c4bb96
Andrea Corna <andrea.corna.ac.91@gmail.com>
2014-04-13 09:44:12 +0200

    add ranging service
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

---
+++ iBeaconOccupancy/src/it/polimi/it/ibeaconoccupancy/services/RangingService.java
@@ -0,0 +1,139 @@
+package it.polimi.it.ibeaconoccupancy.services;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collection;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+import com.radiusnetworks.ibeacon.IBeacon;
+import com.radiusnetworks.ibeacon.IBeaconConsumer;
+import com.radiusnetworks.ibeacon.IBeaconManager;
+import com.radiusnetworks.ibeacon.RangeNotifier;
+import com.radiusnetworks.ibeacon.Region;
+
+public class RangingService extends Service implements IBeaconConsumer{
+	
+	protected static final String TAG = "RangingService";
+	private final IBeaconManager iBeaconManager = IBeaconManager.getInstanceForApplication(this);
+    private Collection<IBeacon> oldInformation = null;
+    private final BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+	
+    @Override
+    public void onStart(Intent intent, int startId) {
+        
+        super.onStart(intent, startId);
+        iBeaconManager.bind(this);
+        Log.d(TAG, "Ranging started");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Ranging finished");
+    }
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+    @Override
+    public void onIBeaconServiceConnect() {
+        iBeaconManager.setRangeNotifier(new RangeNotifier() {
+        @Override 
+        public void didRangeBeaconsInRegion(Collection<IBeacon> iBeacons, Region region) {
+            if (iBeacons.size() > 0) {
+            		compareInformation(iBeacons);
+            		Log.d(TAG,"WE LOVE SANTA CLAUS");
+            }
+        }
+
+        });
+        try {
+            iBeaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {   }
+    }
+    
+    private void post(String url, IBeacon beacon, String idBluetooth, int status){
+    	InputStream inputStream = null;
+        String result = "";
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+            String json = "";
+            JSONObject jsonObject = new JSONObject();
+            String id_beacon = beacon.getProximityUuid()+beacon.getMajor()+beacon.getMinor();
+            jsonObject.accumulate("id_beacon", id_beacon);
+            jsonObject.accumulate("id_device", idBluetooth);
+            jsonObject.accumulate("status", status);
+ 
+            json = jsonObject.toString();
+            StringEntity se = new StringEntity(json);
+ 
+            httpPost.setEntity(se);
+
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+            //logToDisplay(json);
+            HttpResponse httpResponse = httpclient.execute(httpPost);
+
+            inputStream = httpResponse.getEntity().getContent();
+ 
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+ 
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+        	Log.d(TAG,result);
+    
+    }
+    
+    private void compareInformation(Collection<IBeacon> newInformation){
+    	for (IBeacon iBeacon : newInformation) {
+    		post("http://ibeacon.no-ip.org/ibeacon",iBeacon,mBluetoothAdapter.getAddress(),1);
+		}
+    	if(oldInformation != null){
+	    	oldInformation.removeAll(newInformation);
+	    	if(oldInformation.size()>0){
+		    	for (IBeacon iBeacon : oldInformation) {
+		    		post("http://ibeacon.no-ip.org/ibeacon",iBeacon,mBluetoothAdapter.getAddress(),0);
+				}
+		    }
+    	}
+	    oldInformation = newInformation;
+    	
+    }
+
+    
+    protected static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+ 
+        inputStream.close();
+        return result;
+ 
+    }
+
+}
