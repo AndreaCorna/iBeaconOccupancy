def7c2e09d7730199b0dc5d4c9710d309035f2b3
Andrea Corna <andrea.corna.ac.91@gmail.com>
2014-04-16 11:17:21 +0200

    add documentation httphandler.java
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

--- iBeaconOccupancy/src/it/polimi/it/ibeaconoccupancy/http/HttpHandler.java
+++ iBeaconOccupancy/src/it/polimi/it/ibeaconoccupancy/http/HttpHandler.java
@@ -1,36 +1,42 @@
 package it.polimi.it.ibeaconoccupancy.http;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 
 import android.util.Log;
 
 import com.radiusnetworks.ibeacon.IBeacon;
 
-
+/**
+ * This class implements all method in order to interact with the server in order to manage information 
+ * about iBeacons saw by the device.
+ * @author Andrea Corna - Lorenzo Fontana
+ *
+ */
 public class HttpHandler {
 	
 	protected static final String TAG = "HTTP";
 	private String url;
 	
+	/**
+	 * Create an object of the class, setting the base url of the server
+	 * @param url - url of the server root
+	 */
 	public HttpHandler(String url){
 		this.url = url;
 	}
 	
-	
+	/**
+	 * This method does a post on the server with information related to one beacon.
+	 * @param beacon - ibeacon object with all information
+	 * @param idBluetooth - MAC identifier of the device
+	 * @param status - 1 or 0, depending if I see or not a beacon
+	 * @param power - the strength of the signal
+	 */
 	public void postOnRanging(IBeacon beacon, String idBluetooth, int status, int power){
 		int responseCode = 0;
         String id_beacon = beacon.getProximityUuid()+beacon.getMajor()+beacon.getMinor();
@@ -60,6 +66,11 @@ public class HttpHandler {
     
     }
 	
+	/**
+	 * This method does a delete request to server about all information related to
+	 * the MAC identifier passed as an argument
+	 * @param idBluetooth - MAC identifier of the device
+	 */
 	public void postOnMonitoringOut( String idBluetooth){
 		
         String stringDelete = url+"/"+idBluetooth;
