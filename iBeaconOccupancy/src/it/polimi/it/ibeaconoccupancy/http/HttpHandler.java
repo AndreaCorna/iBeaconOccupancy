e75f18a53ee5433f2cfaea3d3cac09d1d6533ee6
Andrea Corna <andrea.corna.ac.91@gmail.com>
2014-04-16 10:03:29 +0200

    remove unused code
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

--- iBeaconOccupancy/src/it/polimi/it/ibeaconoccupancy/http/HttpHandler.java
+++ iBeaconOccupancy/src/it/polimi/it/ibeaconoccupancy/http/HttpHandler.java
@@ -75,16 +75,6 @@ public class HttpHandler {
     
     }
 	
-	private static String convertInputStreamToString(InputStream inputStream) throws IOException{
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while((line = bufferedReader.readLine()) != null)
-            result += line;
- 
-        inputStream.close();
-        return result;
- 
-    }
+
 
 }
