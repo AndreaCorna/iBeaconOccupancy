d22889b7cc4a9d2e4aad206c5c38eed595b22b01
Andrea Corna <andrea.corna.ac.91@gmail.com>
2014-06-11 23:33:25 +0200

    correct ip address
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

--- iBeaconOccupancy/src/it/polimi/it/ibeaconoccupancy/Constants.java
+++ iBeaconOccupancy/src/it/polimi/it/ibeaconoccupancy/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	
 	public static final String ADDRESS_LOGIC_ON_CLIENT = "http://ibeacon.no-ip.org/ibeacon";
 	public static final String ADDRESS_LOGIC_ON_SERVER = "http://ibeacon.no-ip.org/ibeaconserver";
-	public static final String ADDRESS_TRAINING_LEARNING = "http://192.168.0.152/ibeacon/training";
+	public static final String ADDRESS_TRAINING_LEARNING = "http://ibeacon.no-ip.org/ibeacon/training";
 	public static final int UPPER_DISTANCE = 10;
 
 }
