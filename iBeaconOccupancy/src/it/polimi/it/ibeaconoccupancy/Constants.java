7c2c03e701f7ac35c069153c8c108bd21915d659
Andrea Corna <andrea.corna.ac.91@gmail.com>
2014-06-16 12:56:33 +0200

    correct address test learning
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

--- iBeaconOccupancy/src/it/polimi/it/ibeaconoccupancy/Constants.java
+++ iBeaconOccupancy/src/it/polimi/it/ibeaconoccupancy/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	public static final String ADDRESS_LOGIC_ON_CLIENT = "http://ibeacon.no-ip.org/ibeacon";
 	public static final String ADDRESS_LOGIC_ON_SERVER = "http://ibeacon.no-ip.org/ibeaconserver";
 	public static final String ADDRESS_TEST_SERVER_CLIENT = "http://ibeacon.no-ip.org/ibeacon/testClient";
-	public static final String ADDRESS_TEST_SERVER_LEARNING = "http://ibeacon.no-ip.org/ibeacon/testClient";
+	public static final String ADDRESS_TEST_SERVER_LEARNING = "http://ibeacon.no-ip.org/ibeacon/testLearning";
 
 	public static final int UPPER_DISTANCE = 10;
 	
