04980399b8d827d2e957ecfd645429cf7b5f4d2c
Andrea Corna <andrea.corna.ac.91@gmail.com>
2014-07-04 14:40:38 +0200

    add constans
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

---
+++ iBeaconOccupancy/src/it/polimi/it/ibeaconoccupancy/helper/Constants.java
@@ -0,0 +1,9 @@
+package it.polimi.it.ibeaconoccupancy.helper;
+
+public class Constants {
+	
+	public static final String ADDRESS_LOGIC_ON_CLIENT = "http://ibeacon.no-ip.org/ibeacon";
+	public static final String ADDRESS_LOGIC_ON_SERVER = "http://ibeacon.no-ip.org/ibeaconserver";
+	public static final int UPPER_DISTANCE = 10;
+
+}
