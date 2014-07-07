0465c0b82bf46899832f37e81e6cbd86d82859c0
Fons <lord.fontana@gmail.com>
2014-07-07 14:51:39 +0200

    adjusted coeffi and ip
    Signed-off-by: Fons <lord.fontana@gmail.com>

--- iBeaconOccupancy/src/it/polimi/it/ibeaconoccupancy/training/Logic.java
+++ iBeaconOccupancy/src/it/polimi/it/ibeaconoccupancy/training/Logic.java
@@ -47,7 +47,7 @@ public class Logic {
 	 * @return best beacon considering the past
 	 */
 	public void updateInformation(Collection<IBeacon> newInformation){
-		Double coefficent = 0.8;
+		Double coefficent = 0.65;
 		updateStatusBeacon(newInformation);
 		for (IBeacon iBeacon : newInformation) {
 		
