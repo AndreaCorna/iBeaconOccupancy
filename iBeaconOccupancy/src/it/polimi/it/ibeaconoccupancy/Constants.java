a5f53368c82c755b388d0e90108e4255855d0b3e
Fons <lord.fontana@gmail.com>
2014-07-07 10:50:55 +0200

    changed ip and 0.65 coef
    Signed-off-by: Fons <lord.fontana@gmail.com>

--- iBeaconOccupancy/src/it/polimi/it/ibeaconoccupancy/Constants.java
+++ iBeaconOccupancy/src/it/polimi/it/ibeaconoccupancy/Constants.java
@@ -2,10 +2,10 @@ package it.polimi.it.ibeaconoccupancy;
 
 public class Constants {
 	
-	public static final String ADDRESS_LOGIC_ON_CLIENT = "http://ibeacon.no-ip.org/ibeacon";
-	public static final String ADDRESS_LOGIC_ON_SERVER = "http://ibeacon.no-ip.org/ibeaconserver";
-	public static final String ADDRESS_TEST_SERVER_CLIENT = "http://ibeacon.no-ip.org/ibeacon/testClient";
-	public static final String ADDRESS_TEST_SERVER_LEARNING = "http://ibeacon.no-ip.org/ibeacon/testLearning";
+	public static final String ADDRESS_LOGIC_ON_CLIENT = "http://192.168.0.152/ibeacon";
+	public static final String ADDRESS_LOGIC_ON_SERVER = "http://192.168.0.152/ibeaconserver";
+	public static final String ADDRESS_TEST_SERVER_CLIENT = "http://192.168.0.152/ibeacon/testClient";
+	public static final String ADDRESS_TEST_SERVER_LEARNING = "http://192.168.0.152/ibeacon/testLearning";
 
 	public static final int UPPER_DISTANCE = 10;
 	
