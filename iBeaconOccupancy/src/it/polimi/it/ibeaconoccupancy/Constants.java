62a31d452c7b44d16ed1b86e7067ee4af8409636
Andrea Corna <andrea.corna.ac.91@gmail.com>
2014-07-08 17:16:45 +0200

    correct logic
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

--- iBeaconOccupancy/src/it/polimi/it/ibeaconoccupancy/Constants.java
+++ iBeaconOccupancy/src/it/polimi/it/ibeaconoccupancy/Constants.java
@@ -2,9 +2,9 @@ package it.polimi.it.ibeaconoccupancy;
 
 public class Constants {
 	
-	public static final String ADDRESS_LOGIC_ON_CLIENT = "http://ibeacon.no-ip.org/ibeacon";
-	public static final String ADDRESS_LOGIC_ON_SERVER = "http://ibeacon.no-ip.org/ibeaconserver";
-	public static final String ADDRESS_TRAINING_LEARNING = "http://ibeacon.no-ip.org/ibeacon/training";
+	public static final String ADDRESS_LOGIC_ON_CLIENT = "http://192.168.0.105/ibeacon";
+	public static final String ADDRESS_LOGIC_ON_SERVER = "http://192.168.0.105/ibeaconserver";
+	public static final String ADDRESS_TRAINING_LEARNING = "http://192.168.0.105/ibeacon/training";
 	public static final int UPPER_DISTANCE = 10;
 
 }
